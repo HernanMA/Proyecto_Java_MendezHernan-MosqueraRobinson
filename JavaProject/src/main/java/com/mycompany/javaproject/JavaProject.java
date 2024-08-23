5ae2424d1049b2b3079940cabb3cd9a0873b7fb2
Hernan <hernanariza69@gmail.com>
2024-08-23 08:26:51 -0500

    feat: :sparkles:

---
+++ JavaProject/src/main/java/com/mycompany/javaproject/JavaProject.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaproject;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author hernan
+ */
+public class JavaProject {
+
+    public static void main(String[] args) {
+        Conexion conexionDB = Conexion.getInstance();
+        Connection conexion = null;
+
+        try {
+            conexion = conexionDB.conectar();
+
+        } catch (SQLException e) {
+            System.err.println("Error al conectar a la base de datos: " + e.getMessage());
+        } finally {
+            conexionDB.cerrarConexion(conexion);
+        }
+    }
+}
